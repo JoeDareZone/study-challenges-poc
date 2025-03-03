a43868b23cf63505fca020cc44b0890b445193e4
Joe Ulyatt <joewhocodes@gmail.com>
2025-03-03 16:25:02 +0100

    feat: :sparkles: Add tailwind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
 
+import '../global.css'
+
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
