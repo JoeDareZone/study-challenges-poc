8c4bb58702feaacdadd354ed006724218ebbb218
Joe Ulyatt <joewhocodes@gmail.com>
2025-03-05 16:42:01 +0100

    feat: :sparkles: Filter out existing subjects

--- app/subjects.tsx
+++ app/subjects.tsx
@@ -1,12 +1,16 @@
 // SubjectScreen.js
+import { useAllChallenges } from '@/hooks/useAllChallenges'
 import { useGlobalSearchParams, useRouter } from 'expo-router'
 import { Text, TouchableOpacity, View } from 'react-native'
 
 export default function SubjectScreen() {
 	const router = useRouter()
 	const { grade } = useGlobalSearchParams()
+	const { challenges } = useAllChallenges()
 
-	const subjects = ['Math', 'Science', 'History', 'Computers']
+	const subjects = ['Math', 'Science', 'History', 'Computers'].filter(
+		subject => !challenges.some(challenge => challenge.subject === subject)
+	)
 
 	return (
 		<View className='flex-1 bg-gray-100 p-5'>
